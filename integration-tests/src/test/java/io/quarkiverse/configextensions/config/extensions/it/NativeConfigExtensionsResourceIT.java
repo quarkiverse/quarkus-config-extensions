0bcd250ec0517c846cd8e204e9c4806ebe325f2c
George Gastaldi <gegastaldi@gmail.com>
2021-04-01 13:57:43 -0300

    Initial commit

--- integration-tests/src/test/java/io/quarkiverse/configextensions/config/extensions/it/NativeConfigExtensionsResourceIT.java
+++
@@ -1,7 +0,0 @@
-package io.quarkiverse.configextensions.config.extensions.it;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeConfigExtensionsResourceIT extends ConfigExtensionsResourceTest {
-}
---
+++ sample/integration-tests/src/test/java/io/quarkiverse/config/sample/it/NativeConfigExtensionsResourceIT.java
@@ -0,0 +1,7 @@
+package io.quarkiverse.config.sample.it;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeConfigExtensionsResourceIT extends ConfigExtensionsResourceTest {
+}
