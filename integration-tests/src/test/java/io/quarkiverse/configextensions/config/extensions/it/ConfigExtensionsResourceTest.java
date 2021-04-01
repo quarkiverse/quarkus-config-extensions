b21a8865aca0b396799d2f0e7be192a7d9cb929f
George Gastaldi <gegastaldi@gmail.com>
2021-04-01 13:50:01 -0300

    Initial commit

---
+++ integration-tests/src/test/java/io/quarkiverse/configextensions/config/extensions/it/ConfigExtensionsResourceTest.java
@@ -0,0 +1,21 @@
+package io.quarkiverse.configextensions.config.extensions.it;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class ConfigExtensionsResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+                .when().get("/config-extensions")
+                .then()
+                .statusCode(200)
+                .body(is("Hello config-extensions"));
+    }
+}
