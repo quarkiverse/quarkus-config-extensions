66b248bdf0da819375bdda09cbae92555e5b4bed
wansors <wansors@gmail.com>
2021-05-06 23:37:57 +0200

    Added ConfigRoot

---
+++ jdbc/runtime/src/main/java/io/quarkiverse/configextensions/jdbc/runtime/JdbcConfigConfig.java
@@ -0,0 +1,41 @@
+package io.quarkiverse.configextensions.jdbc.runtime;
+
+import java.util.Optional;
+
+import io.quarkus.runtime.annotations.ConfigItem;
+import io.quarkus.runtime.annotations.ConfigPhase;
+import io.quarkus.runtime.annotations.ConfigRoot;
+
+/**
+ * We don't really use this, because these are configurations for the config itself, so it causes a chicken / egg
+ * problem, but we have it so the configurations can be properly documented.
+ *
+ * The config itself is loaded using the ConfigSourceContext on the ConfigSourceFactory
+ */
+@ConfigRoot(name = "quarkus.config.source.jdbc", phase = ConfigPhase.BOOTSTRAP)
+public class JdbcConfigConfig {
+
+    /**
+     * If set to true, the application will attempt to look up the configuration from DB
+     */
+    @ConfigItem(name = "enabled", defaultValue = "true")
+    public boolean enabled;
+
+    /**
+     * Table name for configuration records
+     */
+    @ConfigItem(name = "table", defaultValue = "configuration")
+    public Optional<String> table;
+
+    /**
+     * Name of the column containing the key
+     */
+    @ConfigItem(name = "key", defaultValue = "key")
+    public Optional<String> keyColumn;
+
+    /**
+     * name of the column containing the value
+     */
+    @ConfigItem(name = "value", defaultValue = "value")
+    public Optional<String> valueColumn;
+}
