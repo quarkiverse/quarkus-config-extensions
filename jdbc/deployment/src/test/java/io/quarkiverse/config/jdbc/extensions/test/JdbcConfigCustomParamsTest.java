c7cdc9a6e69e131c86950e008dd201cc897ddc7d
Ghazy Abdallah <ghzooooon@gmail.com>
2022-01-14 10:16:52 +0200

    add tests

--- jdbc/deployment/src/test/java/io/quarkiverse/config/jdbc/extensions/test/JdbcConfigCustomParamsTest.java
+++ jdbc/deployment/src/test/java/io/quarkiverse/config/jdbc/extensions/test/JdbcConfigCustomParamsTest.java
@@ -1,5 +1,11 @@
 package io.quarkiverse.config.jdbc.extensions.test;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
 import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.config.ConfigProvider;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
@@ -20,11 +26,27 @@ public class JdbcConfigCustomParamsTest {
     static final QuarkusUnitTest config = new QuarkusUnitTest().setArchiveProducer(() -> ShrinkWrap
             .create(JavaArchive.class).addAsResource("custom_application.properties", "application.properties"));
 
+    @Inject
+    DataSource dataSource;
+
     @Test
     @DisplayName("Reads a property from config DB")
-    public void readGreetingFromDB() {
+    public void readGreetingFromDB() throws SQLException {
         Config c = ConfigProvider.getConfig();
         String message = c.getValue("greeting.message", String.class);
         Assertions.assertEquals("hello from custom table", message);
+
+        int result = updateConfigValue("greeting.message", "updated hello from custom table");
+        Assertions.assertTrue(result > 0);
+        message = c.getValue("greeting.message", String.class);
+        Assertions.assertEquals("updated hello from custom table", message);
+    }
+
+    private int updateConfigValue(String key, String value) throws SQLException {
+        PreparedStatement updateStatement = dataSource.getConnection()
+                .prepareStatement("UPDATE custom_config c SET c.b = ? WHERE c.a = ?");
+        updateStatement.setString(1, value);
+        updateStatement.setString(2, key);
+        return updateStatement.executeUpdate();
     }
 }
