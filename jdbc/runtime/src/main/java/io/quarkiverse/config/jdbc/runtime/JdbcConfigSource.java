46eeb29f18d5028aeefac0f8f53894e2801a44da
Ghazy Abdallah <ghazy.abdallah@tech11.com>
2022-03-02 14:59:30 +0200

    remove unnecessary try catch blocks

--- jdbc/runtime/src/main/java/io/quarkiverse/config/jdbc/runtime/JdbcConfigSource.java
+++ jdbc/runtime/src/main/java/io/quarkiverse/config/jdbc/runtime/JdbcConfigSource.java
@@ -1,6 +1,5 @@
 package io.quarkiverse.config.jdbc.runtime;
 
-import java.sql.SQLException;
 import java.util.Map;
 import java.util.Set;
 
@@ -19,31 +18,17 @@ public class JdbcConfigSource extends AbstractConfigSource {
 
     @Override
     public Map<String, String> getProperties() {
-        try {
-            return repository.getAllConfigValues();
-        } catch (SQLException e) {
-            log.warn("jdbc-config unable to get properties: " + e.getLocalizedMessage());
-            return Map.of();
-        }
+        return repository.getAllConfigValues();
     }
 
     @Override
     public Set<String> getPropertyNames() {
-        try {
-            return repository.getPropertyNames();
-        } catch (SQLException e) {
-            log.warn("jdbc-config unable to get property names: " + e.getLocalizedMessage());
-            return Set.of();
-        }
+        return repository.getPropertyNames();
     }
 
     @Override
     public String getValue(String propertyName) {
-        try {
-            return repository.getValue(propertyName);
-        } catch (SQLException e) {
-            return null;
-        }
+        return repository.getValue(propertyName);
     }
 
 }
