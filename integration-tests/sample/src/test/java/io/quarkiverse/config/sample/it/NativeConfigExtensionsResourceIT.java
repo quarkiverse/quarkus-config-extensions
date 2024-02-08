60008f89e644023963bd7ff13aab61f1dd024c7f
Roberto Cortez <radcortez@yahoo.com>
2024-02-08 15:00:46 +0000

    Remove sample module

--- integration-tests/sample/src/test/java/io/quarkiverse/config/sample/it/NativeConfigExtensionsResourceIT.java
+++
@@ -1,7 +0,0 @@
-package io.quarkiverse.config.sample.it;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class NativeConfigExtensionsResourceIT extends ConfigExtensionsResourceTest {
-}
